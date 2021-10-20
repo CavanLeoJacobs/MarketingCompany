3b4cbae04655538ae7f5d12d45c2e46aa21172af
CavanLeoJacobs <cavanleojacobs@gmail.com>
2021-10-20 11:04:29 +0200

    origin

---
+++ MarketingCompanyRepository/src/main/java/marketing/company/repo/config/RepositoryConfig.java
@@ -0,0 +1,10 @@
+package marketing.company.repo.config;
+
+public class RepositoryConfig
+{
+  // countable noun.
+    // The config of a computer system is the way in which all its parts,
+    // such as the hardware and software, are connected together in order for the computer to work.
+    // Config is short for ' configuration'.
+
+}
---
+++ MarketingCompanyWebApplication/src/main/java/marketing/company/web/ApplicationMain.java
@@ -0,0 +1,15 @@
+package marketing.company.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMain
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ApplicationMain.class,args);
+
+    }
+
+}
