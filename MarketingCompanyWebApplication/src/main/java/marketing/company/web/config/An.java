9f013b1b01d52c8bec45e0c54c14589748a3f675
CavanLeoJacobs <cavanleojacobs@gmail.com>
2021-11-01 16:45:21 +0200

    origin

---
+++ MarketingCompanyWebApplication/src/main/java/marketing/company/web/config/An.java
@@ -0,0 +1,12 @@
+package marketing.company.web.config;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+
+
+public class An
+{
+
+}
--- MarketingCompanyWebApplication/src/main/java/marketing/company/web/config/WebApplicationConfig.java
+++ MarketingCompanyWebApplication/src/main/java/marketing/company/web/config/WebApplicationConfig.java
@@ -17,8 +17,8 @@ import org.springframework.context.annotation.PropertySource;
         {
                 "marketing.company.web.controller",
                 "marketing.company.web.exceptions",
-
         }
+
 )
 @PropertySource(value = "index.html")
 public class WebApplicationConfig
